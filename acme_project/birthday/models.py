e0810c871d5aa9792818b79ce18f9123a6965d05
Марат Кабардоков <kabardokov.m@yandex.ru>
2025-05-22 15:34:58 +0300

    Настроил автоматическую форму из модели, создал модель

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+
+class Birthday(models.Model):
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=20,
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        help_text='Необязательное поле',
+        max_length=20,
+        blank=True,
+    )
+    birthday = models.DateField(
+        verbose_name='Дата рождения',
+    )
