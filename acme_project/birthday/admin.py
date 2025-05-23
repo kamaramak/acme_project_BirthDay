03a1763ee87fc3af06b8b536177b14f619c2d48e
Марат Кабардоков <kabardokov.m@yandex.ru>
2025-05-23 18:20:16 +0300

    Настроил админку, встроил валидацию на разных уровнях: Битлз, настоящий год рождения, многословное имя

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,22 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+
+@admin.register(Birthday)
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'birthday',
+    )
+    list_display_links = (
+        'first_name',
+        'last_name',
+        'birthday',
+    )
+    search_fields = (
+        'first_name',
+        'last_name',
+    )
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from .validators import real_age
+
 
 class Birthday(models.Model):
     first_name = models.CharField(
@@ -14,4 +16,19 @@ class Birthday(models.Model):
     )
     birthday = models.DateField(
         verbose_name='Дата рождения',
+        validators=(real_age,),
     )
+
+    class Meta:
+        verbose_name = 'день рождения'
+        verbose_name_plural = 'Дни рождения'
+        ordering = ['id']
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
+
+    def __str__(self):
+        return self.first_name + self.last_name
