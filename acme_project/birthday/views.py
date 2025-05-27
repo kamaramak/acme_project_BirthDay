16b5486492067d890f7a0c74db3a30478d3c51e0
Марат Кабардоков <kabardokov.m@yandex.ru>
2025-05-27 17:05:19 +0300

    Закончил настраивать CBV

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,13 +1,11 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView, CreateView, UpdateView, DeleteView, DetailView
+)
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
-
-
-class BirthdayMixin:
-    model = Birthday
-    success_url = reverse_lazy('birthday:list')
+from .utils import calculate_birthday_countdown
 
 
 class BirthdayListView(ListView):
@@ -16,13 +14,27 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-class BirthdayCreateView(BirthdayMixin, CreateView):
+class BirthdayCreateView(CreateView):
+    model = Birthday
     form_class = BirthdayForm
 
 
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
     form_class = BirthdayForm
 
 
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
-    pass
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
