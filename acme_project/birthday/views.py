e83d10e88159397bbf2fb912e74cf244e42c63b4
Марат Кабардоков <kabardokov.m@yandex.ru>
2025-05-27 15:15:26 +0300

    Настроен CBV для просмотра, изменения и удаления данных, создан Миксин, создан новый шаблон для удаления записи

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,10 +1,13 @@
-from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
-from .utils import calculate_birthday_countdown
+
+
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
 
 
 class BirthdayListView(ListView):
@@ -13,44 +16,13 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-    form = BirthdayForm(
-        request.POST or None,
-        files=request.FILES or None,
-        instance=instance,
-    )
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context=context)
+class BirthdayCreateView(BirthdayMixin, CreateView):
+    form_class = BirthdayForm
 
 
-def birthday_list(request):
-    birthdays = Birthday.objects.order_by('id')
-    paginator = Paginator(birthdays, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        'page_obj': page_obj
-    }
-    return render(request, 'birthday/birthday_list.html', context)
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    form_class = BirthdayForm
 
 
-def delete_birthday(request, pk):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {
-        'form': form,
-    }
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
