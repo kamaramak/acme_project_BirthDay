5a33c669196ae6f34374551d6de9c8727ac255cb
Марат Кабардоков <kabardokov.m@yandex.ru>
2025-06-02 17:19:30 +0300

    1

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,3 +1,4 @@
+
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
